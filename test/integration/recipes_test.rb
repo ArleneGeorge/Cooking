503b0f2b3cafaf236b8e1d786663e92a9e2e081c
Arlene George <aemg@live.com>
2018-10-24 01:25:43 +0000

    Add Test Driven Design and index listing

---
+++ test/integration/recipes_test.rb
@@ -0,0 +1,23 @@
+require 'test_helper'
+
+class RecipesTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @chef = Chef.create!(chefname: "Arlene", email: "arlene@example.com")
+    @recipe = Recipe.create(name: "vegtable saute", description: "great vegtable saute add vegtable and oil", chef: @chef)
+    @recipe2 = @chef.recipes.build(name: "chicken saute", description: "great chicken dish")
+    @recipe2.save
+  end
+  
+  test "should get recipes index" do
+    get recipes_path
+    assert_response :success
+  end
+
+  test "should get recipes listing" do 
+    get recipes_path
+    assert_template 'recipes/index'
+    assert_match @recipe.name, response.body
+    assert_match @recipe2.name, response.body
+  end
+end
